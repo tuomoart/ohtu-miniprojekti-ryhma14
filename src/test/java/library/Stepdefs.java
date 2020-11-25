c7bc0316fae11894d61f71148d9eb72f572d0257
Oskari Keränen <oskari.e.keranen@gmail.com>
2020-11-25 15:14:39 +0200

    Add Cucumber test

--- src/test/java/library/Stepdefs.java
+++ src/test/java/library/Stepdefs.java
@@ -1,24 +1,119 @@
 package library;
 
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import library.domain.Book;
+import library.domain.Logic;
+import library.ui.cli.Cli;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
 public class Stepdefs {
+    StubCli cli;
+    List<String> inputs;
 
-    /* not supported until clistub is created */
-    /* @Given("^command new book is selected$")
-    public void startCli() {
+    public class StubCli implements Cli {
+        private List<String> inputs;
+        private List<String> prints;
+        private Logic logic = new Logic();
 
+        public StubCli(List<String> inputs) {
+            this.inputs = inputs;
+            prints = new ArrayList<>();
+        }
+
+        @Override
+        public void start() {
+            while (!inputs.isEmpty()) {
+                String input = readCommand();
+                if (input.equals("-1")) {
+                    break;
+                } else if (input.equals("1")) {
+                    int typeOfTip = readTypeOfTip();
+                    addTip(typeOfTip);
+                } else if (input.equals("2")) {
+                    printBooks();
+                } else {}
+            }
+        }
+
+        public void print(String print) {
+            prints.add(print);
+        }
+
+        public List getPrints() {
+            return prints;
+        }
+
+        @Override
+        public String readCommand() {
+            String input = inputs.get(0);
+            inputs.remove(0);
+            return input;
+        }
+
+        @Override
+        public int readTypeOfTip() {
+            return Integer.parseInt(readCommand());
+        }
+
+        @Override
+        public void addTip(int typeOfTip) {
+            if (typeOfTip == 1) {
+                addBook();
+            }
+        }
+
+        @Override
+        public void addBook() {
+            String nameOfBook = readCommand();
+            String author = readCommand();
+            String year = readCommand();
+            String pages = readCommand();
+            String isbn = readCommand();
+            String message = logic.addBook(nameOfBook, author, year, pages, isbn);
+            print(message);
+        }
+
+        @Override
+        public void printBooks() {
+            List<Book> books = logic.getBooks();
+            for (Book book : books) {
+                print(book.toString());
+            }
+        }
+    }
+
+    @Before
+    public void setup() {
+        inputs = new ArrayList<>();
+    }
+
+    @Given("^command new book is selected$")
+    public void commandNewBook() {
+        inputs.add("1");
+        inputs.add("1");
     }
 
     @When("name {string} and author {string} are entered")
-    public void enterNameAndAuthor() {
-
+    public void enterNameAndAuthor(String name, String author) {
+        inputs.add(name);
+        inputs.add(author);
+        inputs.add("");
+        inputs.add("");
+        inputs.add("");
+        cli = new StubCli(inputs);
+        cli.start();
     }
 
     @Then("Cli will respond with {string}")
-    public void cliRespondCorrect() {
-
-    } */
+    public void cliRespondCorrect(String expectedOutput) {
+        assertTrue(cli.getPrints().contains(expectedOutput));
+    }
 }
--- src/test/resources/library/addingBooks.feature
+++ src/test/resources/library/addingBooks.feature
@@ -1,6 +1,6 @@
 Feature: As a user I want to be able to add a book using Command Line Interface
 
-    ## Scenario: creation of book tip is successful with valid name and author
-        ## Given command new book is selected
-        ## When  name "Sivullinen" and author "Albert Camus" are entered
-        ## Then  Cli will respond with "Kirja lisätty"
+    Scenario: creation of book tip is successful with valid name and author
+        Given command new book is selected
+        When  name "Sivullinen" and author "Albert Camus" are entered
+        Then  Cli will respond with "Kirja lisätty"
