7519d8693f78fd49bf272476e409594692b8149a
Oskari Keränen <oskari.e.keranen@gmail.com>
2020-11-25 14:02:30 +0200

    Disable Cucumber tests

--- src/test/java/library/Stepdefs.java
+++ src/test/java/library/Stepdefs.java
@@ -1,2 +1,24 @@
-package library;public class Stepdefs {
+package library;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Stepdefs {
+
+    /* not supported until clistub is created */
+    /* @Given("^command new book is selected$")
+    public void startCli() {
+
+    }
+
+    @When("name {string} and author {string} are entered")
+    public void enterNameAndAuthor() {
+
+    }
+
+    @Then("Cli will respond with {string}")
+    public void cliRespondCorrect() {
+
+    } */
 }
