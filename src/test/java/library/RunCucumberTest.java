5e44aeb657a272c14f1447bc20f4eb056e7781c5
Oskari Keränen <oskari.e.keranen@gmail.com>
2020-11-25 13:12:49 +0200

    Add RunCucumberTest

--- src/test/java/library/RunCucumberTest.java
+++ src/test/java/library/RunCucumberTest.java
@@ -1,4 +1,15 @@
-package library;
+package ohtu;
 
-public class RunCucumberTest {
-}
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.CucumberOptions.SnippetType;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = "pretty",
+        features = "src/test/resources/library",
+        snippets = SnippetType.CAMELCASE
+)
+
+public class RunCucumberTest {}
