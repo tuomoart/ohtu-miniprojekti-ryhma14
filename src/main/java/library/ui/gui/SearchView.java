c41bedb58633827b855595dbb433c058a3c96029
HiskiR <hiski.r@gmail.com>
2020-12-02 10:24:47 +0200

    refactoring

--- src/main/java/library/ui/gui/SearchView.java
+++ src/main/java/library/ui/gui/SearchView.java
@@ -30,91 +30,77 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import library.domain.Book;
 import library.domain.*;
+
 /**
  *
  * @author hiira
  */
 public class SearchView {
+
     private Button addNewTipButton;
     private Button searchLibraryButton;
     private ComboBox tipDropdownlist;
     private TextField searchBox;
     private Logic logic;
     private Gui gui;
-    private final ObservableList<Book> data;
-            
-    
+    private ObservableList<Book> data;
+
     public SearchView(Gui gui, Logic logic) {
         this.gui = gui;
         this.logic = logic;
-        this.data = FXCollections.observableArrayList(logic.getBooks());
     }
-    
+
     public VBox createBookTable() {
-        
+        this.data = FXCollections.observableArrayList(logic.getBooks());
         TableView<Book> table = new TableView<>();
-        
+
         final Label label = new Label("Kirjat");
 
         table.setEditable(true);
 
-        TableColumn authorCol = new TableColumn("Author");
-        authorCol.setMinWidth(100);
-        authorCol.setCellValueFactory(
-                new PropertyValueFactory<Book, String>("author"));
-
-        TableColumn titleCol = new TableColumn("Title");
-        titleCol.setMinWidth(100);
-        titleCol.setCellValueFactory(
-                new PropertyValueFactory<Book, String>("title"));
-
-        TableColumn pagesCol = new TableColumn("Pages");
-        pagesCol.setMinWidth(100);
-        pagesCol.setCellValueFactory(
-                new PropertyValueFactory<Book, String>("pages"));
-        
-        TableColumn yearCol = new TableColumn("Year");
-        yearCol.setMinWidth(100);
-        yearCol.setCellValueFactory(
-                new PropertyValueFactory<Book, String>("year"));
-        
-        TableColumn isbnCol = new TableColumn("ISBN");
-        isbnCol.setMinWidth(100);
-        isbnCol.setCellValueFactory(
-                new PropertyValueFactory<Book, String>("ISBN"));
-
+        TableColumn authorCol = createTableColumn("Author", "author");
+        TableColumn titleCol = createTableColumn("Title", "title");
+        TableColumn pagesCol = createTableColumn("Pages", "pages");
+        TableColumn yearCol = createTableColumn("Year", "year");
+        TableColumn isbnCol = createTableColumn("ISBN", "ISBN");
         FilteredList<Book> filteredBooks = new FilteredList(data, p -> true);
         table.setItems(filteredBooks);
         table.getColumns().addAll(authorCol, titleCol, yearCol, pagesCol, isbnCol);
-        
+
         ObjectProperty<Predicate<Book>> filter = new SimpleObjectProperty<>();
 
-        filter.bind(Bindings.createObjectBinding(() -> 
-            book -> book.getAuthor().toLowerCase().contains(searchBox.getText().toLowerCase()) ||
-                    book.getTitle().toLowerCase().contains(searchBox.getText().toLowerCase()) ||
-                    book.getPages().toLowerCase().contains(searchBox.getText().toLowerCase()) ||
-                    book.getISBN().toLowerCase().contains(searchBox.getText().toLowerCase()) ||
-                    book.getYear().toLowerCase().contains(searchBox.getText().toLowerCase()), 
-            searchBox.textProperty()));
-        
-        
-        searchBox.setOnKeyReleased(keyEvent ->
-        {
+        filter.bind(Bindings.createObjectBinding(()
+                -> book -> book.getAuthor().toLowerCase().contains(searchBox.getText().toLowerCase())
+                || book.getTitle().toLowerCase().contains(searchBox.getText().toLowerCase())
+                || book.getPages().toLowerCase().contains(searchBox.getText().toLowerCase())
+                || book.getISBN().toLowerCase().contains(searchBox.getText().toLowerCase())
+                || book.getYear().toLowerCase().contains(searchBox.getText().toLowerCase()),
+                searchBox.textProperty()));
+
+        searchBox.setOnKeyReleased(keyEvent
+                -> {
             filteredBooks.predicateProperty().bind(Bindings.createObjectBinding(
-                () -> filter.get()));
+                    () -> filter.get()));
         });
 
         final VBox vbox = new VBox();
         vbox.setSpacing(5);
         vbox.setPadding(new Insets(10, 0, 0, 10));
         vbox.getChildren().addAll(label, table);
-        
+
         return vbox;
+    }
+    
+    private TableColumn createTableColumn(String label, String contents) {
+        TableColumn column = new TableColumn(label);
+        column.setMinWidth(100);
+        column.setCellValueFactory(new PropertyValueFactory<Book, String>(contents));
+        return column;
     }
     
     public Scene createSearchScene() {
         Scene scene = new Scene(new Group());
-        
+
         VBox searchLayout = new VBox();
         searchLayout.setSpacing(10);
         searchLayout.getChildren().add(createMenu());
@@ -123,12 +109,12 @@ public class SearchView {
         searchLayout.getChildren().add(createSearchBox());
         searchLayout.getChildren().add(createBookTable());
         //searchLayout.getChildren().add(createListOfResults());
-        
+
         ((Group) scene.getRoot()).getChildren().addAll(searchLayout);
-        
+
         return scene;
     }
-    
+
     public HBox createMenu() {
         HBox menu = new HBox();
         // this.searchLibraryButton = new Button("Hae lukuvinkkiä");
@@ -136,40 +122,40 @@ public class SearchView {
         menu.setSpacing(10);
         menu.getChildren().add(addNewTipButton);
         //menu.getChildren().add(searchLibraryButton);
-        
+
         return menu;
     }
-    
+
     public Label createTitle() {
         Label title = new Label("Hae lukuvinkkiä");
         title.setFont(Font.font("Arial", 20));
-        
+
         return title;
     }
-    
+
     public HBox createDropDownListForTypeOfTip() {
         HBox dropdownlistLayout = new HBox();
         this.tipDropdownlist = new ComboBox();
-        
+
         tipDropdownlist.getItems().addAll("Kirja");
-        
+
         dropdownlistLayout.getChildren().add(new Label("Vinkkityyppi: "));
         dropdownlistLayout.getChildren().add(this.tipDropdownlist);
-        
+
         return dropdownlistLayout;
     }
-    
+
     public HBox createSearchBox() {
         HBox searchBoxLayout = new HBox();
         searchBoxLayout.setAlignment(Pos.CENTER);
         this.searchBox = new TextField();
-        
+
         searchBox.setPromptText("Hakusana: ");
         searchBoxLayout.getChildren().add(this.searchBox);
-        
+
         return searchBoxLayout;
     }
-    
+
     private Button getCreationButton() {
         Button creationButton = new Button("Lisää uusi lukuvinkki");
         creationButton.setOnAction(new EventHandler<ActionEvent>() {
